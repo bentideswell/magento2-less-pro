4a82a699b48d0fcb1c24bb8c792b837715a570f6
Ben Tideswell <ben@fishpig.co.uk>
2022-09-16 10:06:43 +0100

    1.0.0

---
+++ registration.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * @package FishPig_ThemeBlank
+ * @author  Ben Tideswell (ben@fishpig.com)
+ * @url     https://fishpig.co.uk/magento2-theme
+ */
+use \Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(ComponentRegistrar::MODULE, 'FishPig_LessPro', __DIR__);
